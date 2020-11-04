0c0c04b8b81bf673bf904be16d4db1477438b34f
DimiUA <s.dimi.d@gmail.com>
2020-11-04 14:04:25 +0200

    asdasd

--- www/resources/app/ProtocolHelpers.js
+++ www/resources/app/ProtocolHelpers.js
@@ -358,8 +358,8 @@ Protocol.JT808_KMXXX = JClass(Protocol.Common,{
     },
     initHisPosInfoEx:function(ary, posInfo){
         //posInfo.Battery = ary[21];
-    } 
-      
+    }
+
 });
 Protocol.ClassManager.add("JT808_KMXXX", Protocol.JT808_KMXXX);
 
@@ -378,7 +378,7 @@ Protocol.TIANQIN_LK3GP = JClass(Protocol.Common,{
     initHisPosInfoEx:function(ary, posInfo){
         posInfo.Battery = ary[21];
     },
-    
+
 });
 Protocol.ClassManager.add("TIANQIN_LK3GP", Protocol.TIANQIN_LK3GP);
 
@@ -395,12 +395,58 @@ Protocol.GOT20 = JClass(Protocol.Common,{
         posInfo.Input1Voltage = ary[24];
         posInfo.Input2Voltage = ary[25];
         posInfo.Engine = ary[26];
+        if (posInfo.Voltage >= 0 && posInfo.Voltage <= 3.39) {
+            //blevel = 5;
+            posInfo.Battery = 0;
+        }
+        else if (posInfo.Voltage >= 3.39 && posInfo.Voltage <= 3.45) {
+            //blevel = 4;
+            posInfo.Battery = 20;
+        }
+        else if (posInfo.Voltage > 3.45 && posInfo.Voltage <= 3.54) {
+            //blevel = 4;
+            posInfo.Battery = 40;
+        }
+        else if (posInfo.Voltage > 3.54 && posInfo.Voltage <= 3.72) {
+            //blevel = 3;
+            posInfo.Battery = 60;
+        }
+        else if (posInfo.Voltage > 3.72 && posInfo.Voltage <= 3.8927) {
+            //blevel = 2;
+            posInfo.Battery = 80;
+        }
+        else {
+            posInfo.Battery = 100;
+        }
     },
     initHisPosInfoEx:function(ary, posInfo){
         posInfo.Voltage = ary[21];
         posInfo.Input1Voltage = ary[22];
         posInfo.Input2Voltage = ary[23];
         posInfo.Engine = ary[24];
+        if (posInfo.Voltage >= 0 && posInfo.Voltage <= 3.39) {
+            //blevel = 5;
+            posInfo.Battery = 0;
+        }
+        else if (posInfo.Voltage >= 3.39 && posInfo.Voltage <= 3.45) {
+            //blevel = 4;
+            posInfo.Battery = 20;
+        }
+        else if (posInfo.Voltage > 3.45 && posInfo.Voltage <= 3.54) {
+            //blevel = 4;
+            posInfo.Battery = 40;
+        }
+        else if (posInfo.Voltage > 3.54 && posInfo.Voltage <= 3.72) {
+            //blevel = 3;
+            posInfo.Battery = 60;
+        }
+        else if (posInfo.Voltage > 3.72 && posInfo.Voltage <= 3.8927) {
+            //blevel = 2;
+            posInfo.Battery = 80;
+        }
+        else {
+            posInfo.Battery = 100;
+        }
     },
 });
 Protocol.ClassManager.add("GOT20", Protocol.GOT20);
@@ -465,7 +511,7 @@ Protocol.VT900_DOOR = JClass(Protocol.Common,{
         posInfo.fuel = ary[22];
         posInfo.alt = ary[23];
         posInfo.rfid = ary[24];
-    },    
+    },
 });
 Protocol.ClassManager.add("VT900_DOOR", Protocol.VT900_DOOR);
 
@@ -488,7 +534,7 @@ Protocol.KKS = JClass(Protocol.Common,{
         posInfo.ECT = ary[22];
         posInfo.DTC = ary[23];
         posInfo.MPG = ary[24];
-    },    
+    },
 });
 Protocol.ClassManager.add("KKS", Protocol.KKS);
 
@@ -512,8 +558,12 @@ Protocol.GPT19H = JClass(Protocol.Common,{
         posInfo.Battery = ary[22];
         posInfo.Input2Voltage = ary[23];
         posInfo.Engine = ary[24];
-    },    
-    
+    },
+    createLiNodeOptionsInfo: function(){
+        var ret = "";
+
+        return ret;
+    }
 });
 Protocol.ClassManager.add("GPT19H", Protocol.GPT19H);
 
@@ -529,8 +579,8 @@ Protocol.EELINK_TEMP = JClass(Protocol.Common,{
         posInfo.Voltage = ary[23];
         posInfo.Input1Voltage = ary[24];
         posInfo.Input2Voltage = ary[25];
-        posInfo.Engine = ary[26];        
- 
+        posInfo.Engine = ary[26];
+
     },
     initHisPosInfoEx:function(ary, posInfo){
         posInfo.Voltage = ary[21];
@@ -538,7 +588,7 @@ Protocol.EELINK_TEMP = JClass(Protocol.Common,{
         posInfo.Input2Voltage = ary[23];
         posInfo.Engine = ary[24];
 
-    },    
+    },
 });
 Protocol.ClassManager.add("EELINK_TEMP", Protocol.EELINK_TEMP);
 
@@ -558,3 +608,69 @@ Protocol.PHONE_TRACK = JClass(Protocol.Common,{
     }
 });
 Protocol.ClassManager.add("PHONE_TRACK", Protocol.PHONE_TRACK);
+
+
+Protocol.GPT19H = JClass(Protocol.Common,{
+    STATIC: {
+
+    },
+    constructor: function(arg) {
+        this.initDeviceInfo(arg);
+        this.posInfo = {};
+    },
+    initPosInfoEx:function(ary, posInfo){
+        posInfo.Voltage = ary[23];
+        posInfo.Battery = ary[24];
+        posInfo.Input2Voltage = ary[25];
+        posInfo.Engine = ary[26];
+    },
+    initHisPosInfoEx:function(ary, posInfo){
+        posInfo.Voltage = ary[21];
+        posInfo.Battery = ary[22];
+        posInfo.Input2Voltage = ary[23];
+        posInfo.Engine = ary[24];
+    },
+});
+Protocol.ClassManager.add("GPT19H", Protocol.GPT19H);
+
+Protocol.TIANQIN_LK3GV2 = JClass(Protocol.Common,{
+    STATIC: {
+
+    },
+    constructor: function(arg) {
+        this.initDeviceInfo(arg);
+        this.posInfo = {};
+    },
+    initPosInfoEx:function(ary, posInfo){
+        posInfo.Battery = ary[23];
+        posInfo.LBSWifi = ary[24];
+        posInfo.Voltage = ary[14];
+    },
+    initHisPosInfoEx:function(ary, posInfo){
+        posInfo.Battery = ary[21];
+        posInfo.Voltage = ary[12];
+    },
+});
+Protocol.ClassManager.add("TIANQIN_LK3GV2", Protocol.TIANQIN_LK3GV2);
+
+Protocol.TKXXX = JClass(Protocol.Common,{
+    STATIC: {
+
+    },
+    constructor: function(arg) {
+        this.initDeviceInfo(arg);
+        this.posInfo = {};
+    },
+    initPosInfoEx:function(ary, posInfo){
+
+        posInfo.BatteryVoltage = ary[23];
+        posInfo.ChargeVoltage = ary[24];
+        posInfo.Battery = (ary[25]/ 6 ) * 100;
+    },
+    initHisPosInfoEx:function(ary, posInfo){
+        posInfo.BatteryVoltage = ary[21];
+        posInfo.ChargeVoltage = ary[22];
+        posInfo.Battery = (ary[23]/ 6 ) * 100;
+    }
+});
+Protocol.ClassManager.add("TKXXX", Protocol.TKXXX);
